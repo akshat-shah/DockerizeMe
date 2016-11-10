7c778cf03ca249a7c9c7c59cb8f70ed380d20c59
ashah7 <akshat.shah3001@gmail.com>
2016-11-10 17:06:52 -0500

    bug fix

--- create_dockerfile.sh
+++ create_dockerfile.sh
@@ -1,6 +1,10 @@
-node builder_commands.js
-node detect_technology.js $1 > op.txt
-node populate_dockerfile.js > Dockerfile
-cat Dockerfile
-cp Dockerfile $2
-cp dockerize.sh $2
+if [ -d $2 ]; then
+	cat $2/Dockerfile
+else
+	node builder_commands.js
+	node detect_technology.js $1 > op.txt
+	node populate_dockerfile.js > Dockerfile
+	cat Dockerfile
+	cp Dockerfile $2
+	cp dockerize.sh $2	
+fi
