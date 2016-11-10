5ca20513f7604c4c5192af556f7833a2a673516d
ashah7 <akshat.shah3001@gmail.com>
2016-11-09 23:24:31 -0500

    Added bash script

--- create_dockerfile.sh
+++ create_dockerfile.sh
@@ -1,3 +1,6 @@
 node builder_commands.js
 node detect_technology.js $1 > op.txt
 node populate_dockerfile.js > Dockerfile
+cat Dockerfile
+cp Dockerfile DockerizeMe
+cp dockerize.sh DockerizeMe
---
+++ dockerize.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker build -t ashah7/"$1" .
+docker login -u="ashah7" -p="Docker3001."
+docker push ashah7/"$1"
