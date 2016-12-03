c490823cca27a70ea2f6adf77ce6986a9b2630cc
Akshat Shah <akshat.shah3001@gmail.com>
2016-12-03 15:59:25 -0500

    Update dockerize.sh

--- dockerize.sh
+++ dockerize.sh
@@ -2,4 +2,5 @@
 sudo docker build -t ashah7/"$1" .
 sudo docker login -u="ashah7" -p="Docker3001."
 sudo docker push ashah7/"$1"
+sudo docker rm $(sudo docker ps -a -q);
 sudo docker rmi $(sudo docker images -q);
